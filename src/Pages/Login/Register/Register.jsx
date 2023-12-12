d3e2f458c32eac522e55d6bc7befaa95789dada9
tushar-dev001 <tushar789imran@gmail.com>
2023-12-12 22:29:04 +0600

    password type change

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -129,7 +129,7 @@ const Register = () => {
                 </label>
 
                 <input
-                  type="text"
+                  type="password"
                   {...register("password", {
                     required: true,
                     minLength: 6,
